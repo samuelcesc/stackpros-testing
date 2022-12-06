525a11fda8649c03357f374a3ca28128032ffa7e
Samuel Ojumah <samuelcesc@gmail.com>
2022-12-05 23:43:25 -0500

    Implement simple directory browser class and API

--- api/app/main.py
+++ api/app/main.py
@@ -5,11 +5,52 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+class DirectoryBrowser:
+    """ Simple directory browser class """
+
+    def __init__(self) -> None:
+        self.paths = self.__decode_routes_from_file()
+
+    def filter(self, key) -> Union[list, dict, None]:
+        """ Returns a file object or directory dictionary based on it's key"""
+        if key in self.paths:
+            return self.paths[key]
+        return None
+
+    def __decode_routes_from_file(self) -> dict:
+        """This transforms the format given to a routes format"""
+        # file = open('directory.json', encoding='utf-8')
+
+        # data = json.load(file)
+
+        # file.close()
+
+        return {
+            '/': [{"type": "dir", "name": "home"}],
+            '/home': [{"type": "dir", "name": "myname"}],
+            '/home/myname':  [
+                {"type": "file", "name": "filea.txt"},
+                {"type": "file", "name": "fileb.txt"}
+            ],
+            '/home/myname/filea.txt': {"type": "file", "name": "filea.txt"},
+            '/home/myname/fileb.txt': {"type": "file", "name": "fileb.txt"},
+            '/home/myname/projects': [{"type": "dir", "name": "mysupersecretproject"}],
+            '/home/myname/projects/mysupersecretproject': [{"type": "file", "name": "home"}],
+            '/home/myname/projects/mysupersecretproject/mysupersecretfile': {
+                "type": "file",
+                "name": "mysupersecretfile"
+            }
+        }
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+Browser = DirectoryBrowser()
+
+
+@ app.get("/{full_path:path}")
+def return_path(full_path: str):
+    """ Returns """
+    path = f"/{full_path}"
+    data = Browser.filter(path)
+    if data:
+        return data
+    return {"Not found"}, 404
